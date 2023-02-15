cca84530a379c54bf028ea1ca7c6bfe067f1bd34
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 18:39:39 -0300

    chore:  add regex on customer schema

--- src/schemas/customerSchema.ts
+++ src/schemas/customerSchema.ts
@@ -2,5 +2,9 @@ import Joi from "joi";
 
 export const customerInfos = Joi.object({
   name: Joi.string().max(60).required(),
-  cpf: Joi.string().length(11).required(),
+  cpf: Joi.string()
+    .min(11)
+    .pattern(/^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$/)
+    .required()
+    .messages({ "string.pattern.base": `"cpf" must be 11 digit number` }),
 });
