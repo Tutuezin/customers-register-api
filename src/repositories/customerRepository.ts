300937c8d1fdfc8332ed395a9bb5015ff04df249
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 02:04:10 -0300

    feat: create insertCustomer

--- src/repositories/customerRepository.ts
+++ src/repositories/customerRepository.ts
@@ -4,3 +4,15 @@ import * as customerTypes from "../types/customerTypes";
 export async function findCpf(cpf: string) {
   return await prisma.customer.findUnique({ where: { cpf } });
 }
+
+export async function insertCustomer(
+  customerData: customerTypes.ICustomerData
+) {
+  return await prisma.customer.create({
+    select: {
+      name: true,
+      cpf: true,
+    },
+    data: customerData,
+  });
+}
