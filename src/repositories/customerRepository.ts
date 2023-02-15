90780f34e5f1f86341ddb321f5127da1d563e84f
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 02:03:28 -0300

    feat: create cpf finder on db

--- src/repositories/customerRepository.ts
+++ src/repositories/customerRepository.ts
@@ -1,13 +1,6 @@
 import { prisma } from "../config/database";
+import * as customerTypes from "../types/customerTypes";
 
-export async function insertCustomer(customerData: any) {
-  return "teste";
-
-  /* await prisma.customer.create({
-    select: {
-      name: true,
-      cpf: true,
-    },
-    data: customerData,
-  }); */
+export async function findCpf(cpf: string) {
+  return await prisma.customer.findUnique({ where: { cpf } });
 }
