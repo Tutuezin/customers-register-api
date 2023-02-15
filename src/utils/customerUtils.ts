416334d56bcf5f9962d46075e6eea788581efb8e
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 15:54:17 -0300

    feat: create cpf replacer

--- src/utils/customerUtils.ts
+++ src/utils/customerUtils.ts
@@ -4,3 +4,7 @@ import { Customer } from "@prisma/client";
 export function verifyCpfisCpfRegistered(isCpfRegistered: Customer | null) {
   if (isCpfRegistered) throw conflictError("Cpf");
 }
+
+export function cpfReplacer(cpf: string) {
+  return cpf.replace(/(\d{3}).?(\d{3}).?(\d{3})-?(\d{2})/, "$1$2$3$4");
+}
