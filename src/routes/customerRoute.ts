909b31b7fd89f84b829c4a7c620c887208619e82
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 02:05:58 -0300

    feat: setup customer route with schemas

--- src/routes/customerRoute.ts
+++ src/routes/customerRoute.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { validateSchema } from "@middlewares/validations/schemaValidator";
 import * as customerControler from "@controllers/customerControler";
+import { customerInfos } from "@schemas/customerSchema";
 
 const customerRoute = Router();
 
-customerRoute.post("/customer/cpf", customerControler.customerRegister);
+customerRoute.post(
+  "/customer/cpf",
+  validateSchema(customerInfos),
+  customerControler.customerRegister
+);
 
 export default customerRoute;
