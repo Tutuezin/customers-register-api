d5b2e1aa32bb264c7d3bd466c7f23d7a7f2d602f
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 01:59:06 -0300

    chore: add parameters on customerRegister controller

--- src/controllers/customerControler.ts
+++ src/controllers/customerControler.ts
@@ -4,7 +4,10 @@ import * as customerService from "@services/customerService";
 export async function customerRegister(req: Request, res: Response) {
   const { name, cpf } = req.body;
 
-  const registeredCustomer = await customerService.customerRegister("");
+  const registeredCustomer = await customerService.customerRegister({
+    name,
+    cpf,
+  });
 
   res.status(201).send(registeredCustomer);
 }
