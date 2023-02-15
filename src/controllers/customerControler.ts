39775bf3f80eeee0f9ea6ca4c80569ad7e30ff1e
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-02-15 00:37:16 -0300

    feat: create customer controller

---
+++ src/controllers/customerControler.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import * as customerService from "@services/customerService";
+
+export async function customerRegister(req: Request, res: Response) {
+  const { name, cpf } = req.body;
+
+  const registeredCustomer = await customerService.customerRegister("");
+
+  res.status(201).send(registeredCustomer);
+}
